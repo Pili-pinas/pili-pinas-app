4e5571dd1942ab8417010bf323b4e3d3a42c7c3a
fcpenet <fcpenet@users.noreply.github.com>
2025-06-29 13:17:42 +0000

    deploy: c435f22004bb56758a93093a9e2f650604388ce5

---
+++ assets/_auth.dashboard-B7y0Js_S.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CuKXXcxx.js";const e=function(){return o.jsx("div",{children:'Hello "/_auth/dashboard"!'})};export{e as component};
--- assets/_auth.dashboard-DKfnvsJ8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DVt6ggcC.js";const e=function(){return o.jsx("div",{children:'Hello "/_auth/dashboard"!'})};export{e as component};
